5cad307fc5496a2e28d127f4cba85acd54e1e05a
Vaishnavi Patil <vaishshells@gmail.com>
2021-08-27 17:13:27 +0530

    Add files via upload

--- Sanitiser Dispensing/Sanitiser_dispensing.ino
+++ Sanitiser Dispensing/Sanitiser_dispensing.ino
@@ -1,3 +1,4 @@
+
 #define echoPin 8
 #define trigPin 9
 
@@ -18,6 +19,10 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 Servo Servo1; 
+unsigned long cnt = 0;
+unsigned long StartTime;
+unsigned long CurrentTime;
+unsigned long ElapsedTime;
 
 void setup(){
   Serial.begin(9600);
@@ -26,6 +31,10 @@ void setup(){
   Servo1.attach(servoPin);
   lcd.begin(16, 2);
   lcd.print("SANITISER DISPENSOR");
+  
+  //unsigned long StartTime = 0;
+  //millis();
+
  }
  
 void loop(){
@@ -47,18 +56,25 @@ void loop(){
   Serial.print("Distance : ");
   Serial.print(distance);
   Serial.println(" cm ");
-
   //if distance is less than 10cm 
    if (distance < 10)
    {
+    
+     if(cnt == 1)
+     {
+      Serial.println("Wait for some time");
+     }
+     else
+     {
+      cnt = 1;
     lcd.setCursor(0, 1);
     lcd.print("HAND DETECTED");
     delay(1000);
     Serial.println("Hand Detected");
     // Make servo go to 0 degrees 
    Servo1.write(0); 
-   Serial.println("ZERO Degree");
-   delay(1000); 
+   //Serial.println("ZERO Degree");
+   //delay(1000); 
    // Make servo go to 90 degrees 
    Servo1.write(90); 
    Serial.println("180 Degree");
@@ -69,7 +85,14 @@ void loop(){
    Serial.println("ZERO Degree");
    delay(1000); 
    lcd.setCursor(0, 1);
-    lcd.print("DISPENSED!");
+   lcd.print("DISPENSED!");
+     }
+    
+    
+   }
+   else if (distance > 10)
+   {
+    cnt = 0;
    }
 
   delay(1000);   
